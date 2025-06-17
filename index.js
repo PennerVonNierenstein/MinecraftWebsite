d50bdb4bcb3ebc6a87bf7399dc76655944f12879
PennerVonNierenstein <doktordoofenschmirtz@yahoo.com>
2025-06-17 20:05:14 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const timelabel = document.getElementById("timelabel");
+
+function updateLabel() {
+    window.location.reload();
+}
+
+setInterval(updateLabel, 1000);
+  const date = new Date();
+  let hours =  24 - date.getHours();
+  timelabel.textContent = 98 - date.getDate() + " TAGE UND " + hours + " STUNDEN NOCH!";
+

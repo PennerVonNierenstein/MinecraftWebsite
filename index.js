9fe819dcc51959f7476fd3dd8c7ba7bb565ba485
PennerVonNierenstein <doktordoofenschmirtz@yahoo.com>
2025-06-17 20:09:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function updateLabel() {
     window.location.reload();
 }
 
-setInterval(updateLabel, 1000);
+setInterval(updateLabel, 10000);
   const date = new Date();
   let hours =  24 - date.getHours();
   timelabel.textContent = 98 - date.getDate() + " TAGE UND " + hours + " STUNDEN NOCH!";
